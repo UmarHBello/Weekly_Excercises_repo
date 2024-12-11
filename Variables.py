4d188cc67256704fde6f661faa950a62558df45c
UmarHBello <umarhassan361@gmail,com>
2024-12-11 01:18:08 +0100

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,56 @@
+
+#Day 2: 30 days of programming
+
+import math
+
+
+first_name = "Umar"
+SecondName = "Hassan"
+fullname = "Umar Hassan"
+country = "Nigeria"
+city = "Kano"
+age = 20
+year = 2024
+is_married = True
+is_true = True
+is_light = True
+
+
+print (type(first_name))
+print (type(SecondName))
+print (type(fullname))
+print (type(country))
+print (type(city))
+print (type(age))
+print (type(year))
+print (type(is_married))
+print (type(is_true))
+print (type(is_light))
+
+print(len(first_name))
+print (len(SecondName))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = float(input("Enter the radius of the circle: "))
+area_of_a_circle = math.pi * radius**2
+circum_of_a_circle = math.pi * radius
+
+firstname = str(input("Enter your first name: "))
+
+lastname = str(input("Enter your last name: "))
+
+countryy = str(input("Enter your country name: "))
+
+age = int(input("Enter your age: "))
+
+
